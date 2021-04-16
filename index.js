f0a2bf1bf4af8b30cce035b065af00da998ef5b0
texa2302 <64953436+texa2302@users.noreply.github.com>
2021-04-16 16:43:55 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const mongoose = require('mongoose')
+const exphbs = require('express-handlebars')
+const todoRoutes = require('./routes/todos')
+
+
+const PORT = process.env.PORT || 3000
+
+const app = express()
+const hbs = exphbs.create({
+    defaultLayout: 'main',
+    extname: 'hbs'
+})
+
+app.engine('hbs', hbs.engine)
+app.set('view engine', 'hbs')
+app.set('views', 'views')
+
+app.use(todoRoutes)
+
+async function start (){
+    try{
+        await mongoose.connect('mongodb+srv://artem:Artem2302@cluster0.qeppy.mongodb.net/todos', {
+            useNewUrlParser: true,
+            useFindAndModify: false
+        })
+        app.listen(PORT, () => {
+            console.log('Server has been started ...')
+        })
+    } catch (e) {
+        console.log(e)
+    }
+
+}
+
+start()
