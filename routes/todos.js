1a3e924c7da349058a00204ecef0ef151a6fd5d6
Your Name <you@example.com>
2021-05-18 15:03:37 +0300

    a

---
+++ routes/todos.js
@@ -0,0 +1,31 @@
+const { Router } = require('express')
+const Todo = require('../models/Todo')
+const router = Router()
+
+router.get('/', async (req, res) => {
+    const todos = await Todo.find({}).lean()
+
+    res.render('index', {
+      title: 'Todos list' ,
+      isIndex: true,
+      todos
+    })
+})
+
+router.get('/create', (req, res) => {
+    res.render('create', {
+        title: 'Create todo',
+        isCreate: true
+    })
+})
+
+router.post('/create', async (req, res) => {
+    const todo = new Todo({
+        title: req.body.title
+    })
+
+    await todo.save()
+    res.redirect('/')
+})
+
+module.exports = router
